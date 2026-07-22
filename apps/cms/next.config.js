e790256d22fc196e32c3c022c3946fd2bf42653b
Ntrungz07 <ntrungz0704@gmail.com>
2026-07-22 12:25:43 +0700

    fix: add transpilePackages to next.config.js for internal packages

--- apps/cms/next.config.js
+++ apps/cms/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@repo/ui", "@repo/config", "@repo/types", "@repo/utils"],
   images: {
     remotePatterns: [
       {
